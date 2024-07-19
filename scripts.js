49f59ade677a6782f9f0195ac0a3596b52c4168a
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-07-19 01:57:48 +0100

    mobile facility/ empty input validation

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let mortgageActiveState = document.querySelectorAll('.mortage');
 // const radioButtons = document.querySelectorAll('form input[type="radio"]');
 const radioButtons = document.querySelectorAll('form .label-radio');
 
+
 // console.log(radioButtons);
 mortgageActiveState.forEach(mortgageActive => {
     mortgageActive.addEventListener('click', () => {
@@ -24,3 +25,37 @@ radioButtons.forEach(radio => {
         radio.classList.add('active-radio');
     });
 });
+
+// Calculation selections
+const result = document.querySelector('.result');
+const submit = document.querySelector('button[type="submit"]')
+const mortageAmount = document.getElementById('mortgage-amount');
+const mortageTerm = document.getElementById('Mortgage-term');
+const mortageRate = document.getElementById('Mortgage-rate');
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    let mortageAmountValue = mortageAmount.value.trim();
+    let mortageTermValue = mortageTerm.value.trim();
+    let mortageRateValue = mortageRate.value.trim();
+
+    if (mortageAmountValue && mortageTermValue && mortageRateValue) {
+        result.innerHTML = `
+            <h3 class="result-h3">Your results</h3>
+            <p class="result-p">
+              Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.
+            </p>
+            <div class="total-result">
+              <div class="monthly">
+                <p>Your monthly repayments</p>
+                <h3>£${mortageAmountValue}</h3>
+                </div>
+                <div class="total">
+                <p>Total you'll repay over the term</p>
+                <h3>£${mortageAmountValue}</h3>
+                </div>
+            </div>
+        `
+    } else {
+        alert('please input a value')
+    }
+})
